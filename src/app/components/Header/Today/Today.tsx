be8380469e7d6fcf1e449bf587b36514824aee15
kiyo8jo <hoxq4387@yahoo.co.jp>
2024-12-04 16:50:38 +0900

    headerのTodayコンポーネント作成

--- src/app/components/Header/Today/Today.tsx
+++ src/app/components/Header/Today/Today.tsx
@@ -1,5 +1,8 @@
 const Today = () => {
-  return <div>Today</div>;
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return <div>{`${month}/${day}`}</div>;
 };
 
 export default Today;
