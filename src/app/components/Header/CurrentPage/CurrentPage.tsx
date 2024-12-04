9e063571fed2d48625f41d404fff1e988e1da3ab
kiyo8jo <hoxq4387@yahoo.co.jp>
2024-12-04 16:55:33 +0900

    headerのCurrentPageコンポーネント作成

--- src/app/components/Header/CurrentPage/CurrentPage.tsx
+++ src/app/components/Header/CurrentPage/CurrentPage.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 const CurrentPage = () => {
-  return <div>CurrentPage</div>;
+  let displayPathName = "";
+  const currentPath = usePathname();
+
+  // 現在位置により表示する文字を変更する関数
+  const getCurrentPathName = () => {
+    switch (currentPath) {
+      case "/front":
+        displayPathName = "フロント用画面";
+        break;
+      case "/house":
+        displayPathName = "ハウス用画面";
+        break;
+    }
+  };
+
+  getCurrentPathName();
+
+  return <div>{displayPathName}</div>;
 };
 
 export default CurrentPage;
