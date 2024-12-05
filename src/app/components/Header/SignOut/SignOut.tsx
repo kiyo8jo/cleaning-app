2443cca24dc0a8756a4fd69a0beeaa10f7e4b9c1
kiyo8jo <hoxq4387@yahoo.co.jp>
2024-12-05 17:33:33 +0900

    headerのSignOutコンポーネントがlogin画面で表示されないよう修正

--- src/app/components/Header/SignOut/SignOut.tsx
+++ src/app/components/Header/SignOut/SignOut.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import { signOut } from "@/app/login/actions";
-import styles from './SignOut.module.css'
+import styles from "./SignOut.module.css";
+import { usePathname } from "next/navigation";
 
 const SignOut = () => {
-  return <div onClick={signOut} className={styles.text}>サインアウト</div>;
+  const currentPath = usePathname();
+
+  // login画面以外でsignoutボタンを表示
+  if (currentPath !== "/login") {
+    return (
+      <div onClick={signOut} className={styles.text}>
+        サインアウト
+      </div>
+    );
+  }
 };
 
 export default SignOut;
