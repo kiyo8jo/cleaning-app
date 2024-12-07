dc41f0abf66e5e5d2a2593f1893a90982fb64471
kiyo8jo <hoxq4387@yahoo.co.jp>
2024-12-07 10:27:58 +0900

    houseページの外観作成

--- src/app/components/Main/house/HouseContents/HouseContents.tsx
+++ src/app/components/Main/house/HouseContents/HouseContents.tsx
@@ -1,7 +1,67 @@
+import RoomCard from "../../common/RoomCard/RoomCard";
 import styles from "./HouseContents.module.css";
 
+interface Room {
+  roomNumber: number;
+  roomType: string;
+  cleaningType: string;
+  nowBeds: number;
+  newBeds: number;
+  adult: number;
+  inf: number;
+  kidInf: number;
+}
+
 const HouseContents = () => {
-  return <div className={styles.wrapper}>HouseContents</div>;
+  const testRooms: Room[] = [
+    {
+      roomNumber: 221,
+      roomType: "OTP",
+      cleaningType: "OUT",
+      nowBeds: 3,
+      newBeds: 3,
+      adult: 2,
+      inf: 1,
+      kidInf: 0,
+    },
+    {
+      roomNumber: 223,
+      roomType: "OTP",
+      cleaningType: "OUT",
+      nowBeds: 3,
+      newBeds: 3,
+      adult: 2,
+      inf: 1,
+      kidInf: 0,
+    },
+    {
+      roomNumber: 224,
+      roomType: "OTP",
+      cleaningType: "OUT",
+      nowBeds: 3,
+      newBeds: 3,
+      adult: 2,
+      inf: 1,
+      kidInf: 0,
+    },
+    {
+      roomNumber: 225,
+      roomType: "OTP",
+      cleaningType: "OUT",
+      nowBeds: 3,
+      newBeds: 3,
+      adult: 2,
+      inf: 1,
+      kidInf: 0,
+    },
+  ];
+  return (
+    <main className={styles.main_wrapper}>
+      {testRooms.map((room) => (
+        <RoomCard room={room} key={room.roomNumber} />
+      ))}
+    </main>
+  );
 };
 
 export default HouseContents;
